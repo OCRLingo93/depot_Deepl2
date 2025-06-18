68a14dbfc19468c014c2183cd8aa9360a30c416d
OCRLingo93 <guney.mete@edu.esiee.fr>
2025-06-18 10:40:22 +0200

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,13 +2,12 @@ from PIL import Image
 import pytesseract
 import sys
 
-# (À commenter ou supprimer sur Render, car tesseract sera déjà installé via apt)
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def lire_texte_image(chemin_image):
     try:
         image = Image.open(chemin_image)
-        texte = pytesseract.image_to_string(image, lang='eng')  # ou 'fra' si tu veux
+        texte = pytesseract.image_to_string(image, lang='eng')  
         return texte
     except Exception as e:
         return f"Erreur lors de la lecture de l'image : {e}"
